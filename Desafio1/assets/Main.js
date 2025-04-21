291f1a67c2bfe476b04225504db98c14a621d487
MESIRV3 <leonel.serapio.et7@gmail.com>
2025-04-21 18:52:10 -0300

    first commit

---
+++ Desafio1/assets/Main.js
@@ -0,0 +1,5 @@
+const nombreProducto = "Pancho"
+const precioProducto = 1500
+let cantidadDeseada = prompt("Cuantos Panchos va a comprar (estan 1500 pesos)")
+let precioFinal = precioProducto * cantidadDeseada
+alert ("Serian: " + precioFinal + " pesos")
